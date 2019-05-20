73ece52cdc8068a31c96ce67a0ca64588bd38ca5
Matthias Bolz <matthias.bolz@gmail.com>
2019-05-20 10:57:41 +0200

    paperplane

---
+++ pages/paperplane/index.vue
@@ -0,0 +1,64 @@
+<template lang="pug">
+  .container#paperplanes
+    h2 Papierflieger Wettbewerb
+    p Auf dem Jahresfest der Ev. Lukas-Stiftung am 25.05.2019 veranstalten wir einen eine Papierflieger Wettbewerb. Ziel ist es einen Papierflieger zu bauen und ihn in 3 Versuchen so weit wie möglich fliegen zu lassen.
+    h3 Regeln
+    h4 Teilnehmer
+    p Teilnehmen können Kinder und Jugendliche von 0 - 18 Jahren. Der Flieger muss selbst gebaut und gestartet werden. Kinder bis zum 8. Lebensjahr dürfen sich beim Werfen des Fliegers in zwei Versuchen helfen lassen.
+    h4 Fliegerbau
+    p Der Papierflieger muss aus einem einzigen Stück Papier (Blattgröße maximal A3) vor Ort gefertigt werden. Die Grammatur der Papiers darf dabei 100 g/m2 nicht überscheiten. Das Papier darf geschnitten werden, abgeschnittene Stücke dürfen jedoch nicht weiter verwendet werden. 
+    p Es ist zulässig beim Bau des Papierfliegers ein Stück leichtes durchsichtiges Klebeband mit maximal 25mm Breite und 30mm Länge zu verwenden. Das Stück Klebeband darf in schmale Streifen zerschnitten, aber nur zum Schließen klaffender Falten und Papierlagen und nicht geschichtet, zusammengeklebt, als Ballastgewicht, zur Erzeugung von Flächen oder als Steuerelemente wie z.B. Trimmstreifen oder Klappen verwendet werden. Klebstoff, Büro- oder Heftklammern sind nicht zulässig.
+    h4 Flug
+    p Gestartet wird der Papierflieger durch eine Person (den Werfer), welche ihn ohne Hilfe an einer auf dem Boden markierten Linie (der Startlinie) wirft. Ein Anlauf von 5 m bis zur Startlinie ist erlaubt aber die Benutzung von Rampen oder ähnlicher Hilfsmittel ist untersagt. Der Werfer (einschließlich Füße und Kleidung) darf die Startlinie nicht berühren oder überschreiten.
+    p Die Weitenmessung erfolgt in einer geraden Linie senkrecht zur Startlinie. Gemessen wird der Abstand von der Startlinie bis zu der Stelle, an der eine Parallele der Startlinie diese Messlinie schneidet. Maßgeblich ist der Punkt, an welchem der Papierflieger auf oder über dem Boden zur Ruhe kommt.
+    h4 Wertung
+    p Es sind insgesamt drei Versuche gestattet. Gewertet wird der weiteste dieser drei Flüge. Jeder Zentimeter zählt einen Punkt. 
+    p Zusätzlich wird noch das optische Design des Fliegers mit 0-100 Punkten bewertet. 
+    h3 Preise
+    table.pricetable
+      tr
+        th.first Platz
+        th 0-12 Jahre
+        th 13-18 Jahre
+      tr
+        td.first 1
+        td Parrot Hydrofoil Drone
+        td Parrot Swing minidrone
+      tr
+        td.first 2
+        td Rocket Race Car
+        td Aqua-Star Wasserrakete    
+      tr
+        td.first 3
+        td Felix-IQ Flieger
+        td Felix-IQ XL Flieger
+      tr
+        td.first 4
+        td Mini-RC Car
+        td Mini-RC Car
+      tr
+        td.first 5
+        td Buch "100 Motivböge für Papierflieger"
+        td Buch "Backpulver und Gurkenstrom"
+
+
+
+</template>
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+.container {
+  margin-top: 32px;
+  margin-bottom: 48px;
+}
+.pricetable td{
+  width: 40%;
+}
+td.first {
+  width: 10%
+}
+</style>
